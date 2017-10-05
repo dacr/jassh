352aac528f838fdf5a5ea0243ba87cbe2deee36b
crosson.david@gmail.com <crosson.david@gmail.com>
2017-10-05 23:02:17 +0200

    fix username mistake with intricated tunnels

--- src/main/scala/fr/janalyse/ssh/SSHConnectionManager.scala
+++ src/main/scala/fr/janalyse/ssh/SSHConnectionManager.scala
@@ -60,12 +60,12 @@ class SSHConnectionManager(accesses:List[AccessPath]) {
           val currentPosition = previousPosition:+endpoint
           val bounce = bouncers.get(currentPosition) match {
             case Some(bounce) =>
-              if (logger.isDebugEnabled()) logger.debug(s"$aname : reuse bounce for ${endpoint.host}:${endpoint.port}")
+              if (logger.isDebugEnabled()) logger.debug(s"$aname : reuse bounce for ${endpoint.username}@${endpoint.host}~${endpoint.port}")
               bounce
             case None =>
-              if (logger.isDebugEnabled()) logger.debug(s"$aname : create bounce for ${endpoint.host}:${endpoint.port}")
+              if (logger.isDebugEnabled()) logger.debug(s"$aname : create bounce for ${endpoint.username}@${endpoint.host}~${endpoint.port}")
               val proxy = through.map(p => new ProxyHTTP(p.host, p.port))
-              val opts = SSHOptions(localEndPoint.get.host, username = endpoint.username, port = localEndPoint.get.port, proxy = proxy)
+              val opts = SSHOptions(localEndPoint.get.host, username = localEndPoint.get.username, port = localEndPoint.get.port, proxy = proxy)
               val ssh = SSH(opts)
               val newPort = ssh.remote2Local(endpoint.host, endpoint.port)
               val newLocalEndPoint = SshEndPoint("127.0.0.1", username = endpoint.username, port = newPort)
@@ -79,10 +79,10 @@ class SSHConnectionManager(accesses:List[AccessPath]) {
           val currentPosition = previousPosition:+endpoint
           val bounce = bouncers.get(currentPosition) match {
             case Some(bounce) =>
-              if (logger.isDebugEnabled()) logger.debug(s"$aname : reuse bounce for ${endpoint.host}:${endpoint.port}")
+              if (logger.isDebugEnabled()) logger.debug(s"$aname : reuse bounce for ${endpoint.username}@${endpoint.host}~${endpoint.port}")
               bounce
             case None =>
-              if (logger.isDebugEnabled()) logger.debug(s"$aname : create bounce for ${endpoint.host}:${endpoint.port}")
+              if (logger.isDebugEnabled()) logger.debug(s"$aname : create bounce for ${endpoint.username}@${endpoint.host}~${endpoint.port}")
               val proxy = through.map(p => new ProxyHTTP(p.host, p.port))
               val opts = SSHOptions(endpoint.host, username = endpoint.username, port = endpoint.port, proxy = proxy)
               val ssh = SSH(opts)
@@ -101,11 +101,14 @@ class SSHConnectionManager(accesses:List[AccessPath]) {
           sshers.get(position) match {
             case None =>
               val endpoint = localEndPoint.get
+              if (logger.isDebugEnabled()) logger.debug(s"$aname : create terminal endpoint for ${endpoint.username}@${endpoint.host}~${endpoint.port}")
               val opts=SSHOptions(endpoint.host, username=endpoint.username, port=endpoint.port)
               val ssh = SSH(opts)
               sshers += position->ssh
               ssh
-            case Some(ssh) => ssh
+            case Some(ssh) =>
+              if (logger.isDebugEnabled()) logger.debug(s"$aname : reuse terminal endpoint for ${ssh.options.username}@${ssh.options.host}~${ssh.options.port}")
+              ssh
           }
 
         // ----------------------------------------------------------------
